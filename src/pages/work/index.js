70bb5eba2e34dc1fb84bc7b59357bd0ecbd829fd
tyrellbain <contact@tyrellbain.com>
2020-06-24 07:18:43 -0400

    Add functionality to work page

--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,16 +1,60 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import SVGText from '../../components/SVGText/SVGText';
 
 import classnames from 'classnames';
+import debounce from 'lodash.debounce';
+import styles from './Work.module.scss';
+
 const { works } = require('../../data/work.json');
+const slideCount = works.length;
+const SCROLL_THRESHOLD = 1;
 
 function Work() {
+  const [activeSlide, setActiveSlide] = useState(1);
+
+  const nextSlide = useCallback(() => {
+    if (activeSlide !== slideCount) {
+      setActiveSlide(activeSlide + 1);
+    }
+  }, [setActiveSlide, activeSlide]);
+
+  const prevSlide = useCallback(() => {
+    if (activeSlide !== 1) {
+      setActiveSlide(activeSlide - 1);
+    }
+  }, [activeSlide, setActiveSlide]);
+
+  const onwheel = debounce(({ wheelDeltaY }) => {
+    if (wheelDeltaY < -SCROLL_THRESHOLD) {
+      nextSlide();
+    } else if (wheelDeltaY > SCROLL_THRESHOLD) {
+      prevSlide();
+    }
+  }, 250);
+
+  useEffect(() => {
+    window.addEventListener('wheel', onwheel);
+    return () => window.removeEventListener('wheel', onwheel);
+  }, [onwheel]);
+
   return (
-    <div>
-      I've worked on neat project for the following companies,
-      {works.map((work) => (
-        <span> {work.name}, </span>
-      ))}
+    <div className={classnames(styles.root)}>
+      <div className={classnames(styles.container)} style={{ transform: `translateY(-${(activeSlide - 1) * 100}%)` }}>
+        {works.map((work) => (
+          <section className={classnames(styles.project)} key={work.name}>
+            <div className={classnames(styles.content)}>
+              <h2>{work.name}</h2>
+              <p>{work.description}</p>
+              <span>{work.role}/</span>
+              <span>{work.year}</span>
+            </div>
+            <div className={classnames(styles.imgContainer)}>
+              <img src={work.image} />
+            </div>
+          </section>
+        ))}
+      </div>
+      <div className={classnames(styles.slideCount)}>{`${activeSlide}/${slideCount}`}</div>
     </div>
   );
 }
