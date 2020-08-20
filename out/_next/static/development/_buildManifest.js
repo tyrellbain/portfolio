0000d0347b3ea14453ca53c5eb0bed2b0b31c534
tyrellbain <contact@tyrellbain.com>
2020-08-20 09:05:16 -0400

    prod ready

---
+++ out/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/work":["static\u002Fchunks\u002Fpages\u002Fwork.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
